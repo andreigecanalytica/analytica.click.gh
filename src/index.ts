bcb73d4133ce33974f90c364f5e381dfd053a40d
andrei <andreigec@hotmail.com>
2023-07-07 23:01:54 +1000

    w

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ async function run(): Promise<void> {
   const ANALYTICA_TOKEN: string = core.getInput('ANALYTICA_TOKEN');
   const GITHUB_CONTEXT: string = core.getInput('GITHUB_CONTEXT');
   if (!ANALYTICA_TOKEN || !GITHUB_CONTEXT) {
-    core.warning('missing values');
+    core.warning('missing values1');
     return;
   }
   // return runParams({ ANALYTICA_TOKEN, GITHUB_CONTEXT });
